823eed16e2f006365746460c0df3a0faf5402e9a
yerzhan massalin <yerzhik@gmail.com>
2017-11-11 21:16:22 +0600

    data seperated by trials: must check

---
+++ dataseparator.m
@@ -0,0 +1,23 @@
+len = numel(mydata);
+maindir = 'data_by_trials';
+classdir = './data_by_trials/%s/';
+speeddir = './data_by_trials/%s/%d/';
+template = './data_by_trials/%s/%d/%s';
+dirtemplate = './data_by_trials/%d/%s/*.mat';
+mkdir(maindir);
+for i=1:len
+    str = mydata{i}.filename;
+    str = strrep(str, '_again', '');
+    str = strrep(str, '.csv', '');
+    str = regexprep(str, '_vel\d*', '');
+    
+    classname = str;
+    speed = abs(mydata{i}.velocity); 
+    
+    mkdir(sprintf(classdir, classname));
+    mkdir(sprintf(speeddir, classname, speed));
+    numbers = numel(dir(sprintf(dirtemplate, classname, speed))) + 1;
+    trialname = sprintf('trial%d.mat', numbers);
+    tosave = mydata{i};
+    save(sprintf(template, classname, speed, trialname), 'tosave');
+end;
